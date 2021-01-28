ef9e7b0e971cf61c4244e018ffb43d26c7b36492
Evan Pipta <evan.pipta@priceline.com>
2021-01-27 23:55:03 -0500

    Add some text to console logs

--- src/index.mjs
+++ src/index.mjs
@@ -5,14 +5,14 @@ const account1 = loadAccount('./keypair.json')
 const account2 = loadAccount('./keypair2.json')
 
 const txSig = await sendSol({
-  source: account1,
-  dest: account2,
+  source: account2,
+  dest: account1,
   amount: 1,
   connection,
   commitment: 'root',
 })
 
-console.log(`${lampToSol(await connection.getBalance(account1.publicKey))} Sol`)
-console.log(`${lampToSol(await connection.getBalance(account2.publicKey))} Sol`)
+console.log(`Account 1: ${lampToSol(await connection.getBalance(account1.publicKey))} Sol`)
+console.log(`Account 2: ${lampToSol(await connection.getBalance(account2.publicKey))} Sol`)
 
 process.exit(0)
